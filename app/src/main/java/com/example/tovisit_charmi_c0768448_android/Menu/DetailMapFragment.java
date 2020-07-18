9586c507eaddb3a0c11872b8f07c1d9e14bca0a7
Charmi <c0768448@mylambton.ca>
2020-07-18 12:36:58 -0400

    add map fragment

---
+++ app/src/main/java/com/example/tovisit_charmi_c0768448_android/Menu/DetailMapFragment.java
@@ -0,0 +1,16 @@
+package com.example.tovisit_charmi_c0768448_android.Menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tovisit_charmi_c0768448_android.R;
+
+public class DetailMapFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_map_fragment);
+    }
+}
