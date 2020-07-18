7f0093de59a756a994186562f845f3dfe7bc9ca7
Charmi <c0768448@mylambton.ca>
2020-07-18 12:18:17 -0400

    add dashboard fragment

---
+++ app/src/main/java/com/example/tovisit_charmi_c0768448_android/Menu/DashboardFragment.java
@@ -0,0 +1,14 @@
+package com.example.tovisit_charmi_c0768448_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_fragment);
+    }
+}
