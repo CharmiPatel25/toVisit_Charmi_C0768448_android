d2809fdd3bdfd5ef1e50b8bb37ecd4c77dc63e8e
Charmi <c0768448@mylambton.ca>
2020-07-17 20:06:26 -0400

    Initial commit

---
+++ app/src/main/java/com/example/tovisit_charmi_c0768448_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tovisit_charmi_c0768448_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
